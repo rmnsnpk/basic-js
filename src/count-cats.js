13a5c53bcfc36407db494545566c23327833cfa3
Roman <snapko0063@gmail.com>
2021-02-08 12:07:25 +0300

    feat: month

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  if(Array.isArray(matrix)== false || Array.isArray(matrix[0])== false){
+    return 0;
+  }
+  let catCounter = 0;
+  for(let i = 0; i<matrix.length; i++){
+    for(let j = 0; j<matrix[i].length; j++){
+      if(matrix[i][j] === '^^'){
+        catCounter++;
+      } 
+    }
+  }
+  return catCounter;
 };
