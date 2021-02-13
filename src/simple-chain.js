44c557ca44e3502a7785562e22b28d9f8ef84461
rmnsnpk <69847180+rmnsnpk@users.noreply.github.com>
2021-02-13 12:40:56 +0300

    Add files via upload

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  array: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.length
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.array.push(`( ${value} )`)
+    return this
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(typeof position != "number"){
+      this.array = []
+      throw Error()
+    }
+    if(position >= this.array.length || position <= 0){
+      this.array = []
+      throw Error()
+    }
+    this.array.splice(position - 1,1)
+    return this
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.array.reverse()
+    return this
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let chain = this.array.join('~~')
+    this.array = []
+    return chain
   }
 };
 
